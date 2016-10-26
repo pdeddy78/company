09318fc9a8fcfa07b4e52adad78d0cbf099bb8ed
Deddy Pratama <pdeddy78@gmail.com>
2016-10-26 09:36:40 +0700

    upload files

---
+++ config/web.php
@@ -0,0 +1,133 @@
+<?php
+$conf = parse_ini_file('/var/conf.ini', true);
+$params = require(__DIR__ . '/params.php');
+
+$config = [
+    'id' => 'basic',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+	'language' => 'id',
+    'timezone' => 'Asia/Jakarta',
+    'components' => [
+		'assetManager' => [
+            'bundles' => [
+                'yii\web\JqueryAsset' => [
+                    'sourcePath' => null,   // do not publish the bundle
+                    'js' => [
+                        'https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js',
+                    ]
+                ],
+				'yii\bootstrap\BootstrapAsset' => [
+					'css' => [
+						'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
+					],
+				],
+				'yii\bootstrap\BootstrapPluginAsset' => [
+					'js' => [
+						'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js',
+					],
+				],
+				'yii\bootstrap\BootstrapThemeAsset' => [
+					'css' => [
+						'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css',
+					],
+				],
+            ],
+        ],
+        'request' => [
+			'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ],
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'PqWL5Ba58yeJTpwHj2m2sL9ZaJCf0kQl',
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'user' => [
+            'identityClass' => 'app\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'prefix' => function ($message) {
+                        $user = Yii::$app->has('user', true) ? Yii::$app->get('user') : null;
+                        $userID = $user ? $user->getId(false) : '-';
+                        return "[$userID]";
+                    },
+                    'levels' => ['error', 'warning'],
+                    'categories' => [
+                        'yii\db\*',
+                        'yii\web\HttpException:*',
+                        'yii\swiftmailer\Logger::add'
+                    ],
+                    'except' => [
+                        'yii\web\HttpException:404',
+                    ],
+                ],
+                [
+                    'class' => 'yii\log\EmailTarget',
+                    'levels' => ['error'],
+                    'categories' => ['yii\db\*'],
+                    'message' => [
+                       'from' => [$conf['email_official']],
+                       'to' => [$conf['email_admin']],
+                       'subject' => 'Database errors at PESSTA+',
+                    ],
+                ],
+            ],
+        ],
+        'db' => require(__DIR__ . '/db.php'),
+		'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@app/mail',
+            'useFileTransport' => false,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => $conf['smtp_host'],
+                'username' => $conf['smtp_username'],
+                'password' => $conf['smtp_password'],
+                'port' => '2525',
+                'encryption' => 'tls',
+            ],
+        ],        
+        'urlManager' => [
+			'class' => 'yii\web\UrlManager',
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+            ],
+        ],
+        
+    ],
+    'params' => $params,
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+	/*
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+	*/
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
