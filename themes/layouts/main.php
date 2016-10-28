7c7bf14008831a1f8633aac27fc7818f689e66fa
Deddy Pratama <pdeddy78@gmail.com>
2016-10-28 14:00:11 +0700

    add breadcrumb

--- themes/layouts/main.php
+++ themes/layouts/main.php
@@ -119,6 +119,15 @@ use app\assets\VendorAsset;
             <!-- except Landing Page -->
             <?php if(Yii::$app->controller->action->id != 'index' ) { ?>
             <section class="page-header">
+				<div class="container">
+						<div class="row">
+							<div class="col-md-12">
+								 <?= Breadcrumbs::widget([
+            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+        ]) ?>
+							</div>
+						</div>
+					</div>
                 <div class="container">
                     <div class="row">
                         <div class="col-md-12">
